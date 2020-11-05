387cc34a147c1497246d0f6f1cba2a9b6fc1bf63
Dmitriy Vinokurov <gim6626@gmail.com>
2020-11-05 13:53:43 +0500

    Crontab help tip

--- fngs/gatherer/management/commands/gatherfromsources.cron.sh
+++ fngs/gatherer/management/commands/gatherfromsources.cron.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Add "0 * * * * /ABSOLUTE_PATH_TO_SCRIPT/gatherfromsources.cron.sh" to your crontab
 
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$SCRIPT_DIRECTORY/../../../../"
