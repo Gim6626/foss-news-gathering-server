0a429d989941c5ef1ff9021005849c85d54c28c6
Dmitriy Vinokurov <gim6626@gmail.com>
2021-04-05 21:22:02 +0500

    Debug disable

--- fngs/fngs/settings.py
+++ fngs/fngs/settings.py
@@ -24,7 +24,7 @@ from .secret import DJANGO_SECRET_KEY
 SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
