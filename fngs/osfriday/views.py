c0293b569b63bd6527a430430f0ab32ee86176a6
Dmitriy Vinokurov <gim6626@gmail.com>
2021-07-18 10:51:01 +0500

    OS Friday RSS content type fix

--- fngs/osfriday/views.py
+++ fngs/osfriday/views.py
@@ -8,6 +8,7 @@ from gatherer.models import (
 class OsFridayFeedView(ListView):
     model = DigestRecord
     template_name = 'os_friday_rss.xml'
+    content_type = 'application/rss+xml'
 
     def get_queryset(self):
         objects = DigestRecord.objects.filter(projects__in=Project.objects.filter(name='OS Friday')).order_by('-dt')[:1000]
