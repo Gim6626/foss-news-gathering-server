efdc58eaa252da171a23ab342c765743e1d3c8e1
Dmitriy Vinokurov <gim6626@gmail.com>
2021-08-21 14:06:27 +0500

    Added Telegram Bot User by tid endpoint

--- fngs/tbot/views.py
+++ fngs/tbot/views.py
@@ -3,7 +3,9 @@ from rest_framework import (
     viewsets,
     permissions,
     mixins,
+    status,
 )
+from rest_framework.response import Response
 
 from gatherer.serializers import *
 from common.permissions import *
@@ -50,3 +52,26 @@ class TelegramBotOneRandomNotCategorizedFossNewsDigestRecordViewSet(mixins.ListM
         not_categorized_by_this_user_digest_records = DigestRecord.objects.filter(state='UNKNOWN', projects__in=(Project.objects.filter(name='FOSS News'))).exclude(pk__in=[a.digest_record.pk for a in categorized_by_this_user_digest_records_attempts]).order_by('-dt')
         random_record = random.choice(not_categorized_by_this_user_digest_records)
         return [random_record]
+
+
+class TelegramBotUserByTidViewSet(mixins.ListModelMixin,
+                                  viewsets.GenericViewSet):
+    permission_classes = [permissions.IsAdminUser | TelegramBotReadOnlyPermission]
+
+    def list(self, request, *args, **kwargs):
+        tid = request.query_params.get('tid', None)
+        if not tid:
+            return Response({'error': '"tid" option is required'},
+                            status=status.HTTP_400_BAD_REQUEST)
+        telegram_bot_users = TelegramBotUser.objects.filter(tid=tid)
+        if not telegram_bot_users:
+            return Response({'error': 'Telegram bot users not found'},
+                            status=status.HTTP_404_NOT_FOUND)
+        telegram_bot_user = telegram_bot_users[0]
+        return Response({
+                            # TODO: Use serializer
+                            'id': telegram_bot_user.id,
+                            'tid': telegram_bot_user.tid,
+                            'username': telegram_bot_user.username,
+                        },
+                        status=status.HTTP_200_OK)
