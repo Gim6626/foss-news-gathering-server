61c1b46d1133f47a12f6a125b1088cd3128b70d7
Dmitriy Vinokurov <gim6626@gmail.com>
2021-08-25 07:27:55 +0500

    FNGS start script

---
+++ start-fngs.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$SCRIPT_DIRECTORY"
+source env/bin/activate
+cd fngs
+python3 manage.py runserver 2>&1 | tee -a server.log
