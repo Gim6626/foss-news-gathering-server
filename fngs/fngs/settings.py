ad745d409ae06a06a37f3fd120f82260f3b6f6e7
Dmitriy Vinokurov <gim6626@gmail.com>
2021-08-31 08:57:37 +0500

    Replaced DB engine with PostgreSQL

--- fngs/fngs/settings.py
+++ fngs/fngs/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from .secret import DJANGO_SECRET_KEY
+from .secret import (
+    DJANGO_SECRET_KEY,
+    DB_PASSWORD,
+)
 SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -92,9 +95,14 @@ WSGI_APPLICATION = 'fngs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'CONN_MAX_AGE': 0,
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'localhost',
+        'NAME': 'fossnews',
+        'PASSWORD': DB_PASSWORD,
+        'PORT': '5432',
+        'USER': 'postgres',
+    },
 }
 
 
