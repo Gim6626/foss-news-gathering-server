e17b05fb1028eb15c170421ca0cf099a9c765833
Dmitriy Vinokurov <gim6626@gmail.com>
2021-10-21 13:25:02 +0500

    TG-39 Added missing additional_url field to digest records categorized by tbot view

--- fngs/gatherer/views.py
+++ fngs/gatherer/views.py
@@ -183,6 +183,7 @@ class DigestRecordsCategorizedByTbotViewSet(mixins.ListModelMixin, GenericViewSe
                     'source': categorization_attempt.digest_record.source.name,
                     'title': categorization_attempt.digest_record.title,
                     'url': categorization_attempt.digest_record.url,
+                    'additional_url': categorization_attempt.digest_record.additional_url,
                     'digest_issue': categorization_attempt.digest_record.digest_issue,
                     'is_main': categorization_attempt.digest_record.is_main,
                     'content_type': categorization_attempt.digest_record.content_type,
