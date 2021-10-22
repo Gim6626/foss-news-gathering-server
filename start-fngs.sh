570477b561f66d753531da4a6aa381ab22e05b1b
Dmitriy Vinokurov <gim6626@gmail.com>
2021-10-22 08:11:22 +0500

    TG-41 Added port parameter to start script to support multiple running servers

--- start-fngs.sh
+++ start-fngs.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
+if [ -z $1 ]
+then
+  echo "Usage: $0 PORT"
+  exit 1
+fi
+set -e
+set -u
+PORT=$1
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$SCRIPT_DIRECTORY"
 source env/bin/activate
 cd fngs
-gunicorn -w $(( 2 * `cat /proc/cpuinfo | grep 'core id' | wc -l` + 1 )) fngs.wsgi
+gunicorn --bind=0.0.0.0:$PORT -w $(( 2 * `cat /proc/cpuinfo | grep 'core id' | wc -l` + 1 )) fngs.wsgi
