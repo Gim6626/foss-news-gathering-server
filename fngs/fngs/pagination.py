35999d785a7d1ee8535f4e8fc6def15175b7a6e7
Dmitriy Vinokurov <gim6626@gmail.com>
2021-11-10 07:34:37 +0500

    TG-69 Increased max page size to minimize network requests

--- fngs/fngs/pagination.py
+++ fngs/fngs/pagination.py
@@ -5,7 +5,7 @@ from rest_framework.status import *
 
 class Pagination(PageNumberPagination):
     page_size = 50
-    max_page_size = 100
+    max_page_size = 500
     page_size_query_param = 'page_size'
 
     def get_paginated_response(self, data):
