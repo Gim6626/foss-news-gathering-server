50f2a6eecb3fd88fcb81c9d158d5e93e8ed24705
Dmitriy Vinokurov <gim6626@gmail.com>
2021-12-12 09:32:29 +0500

    TG-80 Added basenames to new API endpoints

--- fngs/gatherer/urls_v2.py
+++ fngs/gatherer/urls_v2.py
@@ -10,42 +10,55 @@ urlpatterns = [
 router = routers.DefaultRouter()
 router.register('digest-record',
                 # OLD: 'digest-records',
-                DigestRecordViewSet)
+                DigestRecordViewSet,
+                basename='digest-record')
 router.register('digest-record/not-categorized',
                 # OLD: 'new-digest-records'
-                NewFossNewsDigestRecordViewSet)
+                NewFossNewsDigestRecordViewSet,
+                basename='digest-record/not-categorized')
 router.register('digest-record/not-categorized/oldest',
                 # OLD: 'one-new-foss-news-digest-record'
-                OldestNotCategorizedDigestRecordViewSet)
+                OldestNotCategorizedDigestRecordViewSet,
+                basename='digest-record/not-categorized/oldest')
 router.register('digest-record/not-categorized/count',
                 # OLD: 'not-categorized-digest-records-count'
-                NotCategorizedDigestRecordsCountViewSet)
+                NotCategorizedDigestRecordsCountViewSet,
+                basename='digest-record/not-categorized/count')
 router.register('digest-record/detailed',
                 # OLD: 'specific-digest-records',
-                DetailedDigestRecordViewSet)
+                DetailedDigestRecordViewSet,
+                basename='digest-record/detailed')
 router.register('similar-digest-record',
                 # OLD: 'similar-digest-records',
-                SimilarDigestRecordsViewSet)
+                SimilarDigestRecordsViewSet,
+                basename='similar-digest-record')
 router.register('similar-digest-record/detailed',
                 # OLD: 'similar-digest-records-detailed',
-                SimilarDigestRecordsDetailedViewSet)
+                SimilarDigestRecordsDetailedViewSet,
+                basename='similar-digest-record/detailed')
 router.register('digest-record/similar',
                 # OLD: 'digest-records-looking-similar',
-                DigestRecordsLookingSimilarViewSet)
+                DigestRecordsLookingSimilarViewSet,
+                basename='digest-record/similar')
 router.register('project',
                 # OLD: 'projects'
-                ProjectViewSet)
+                ProjectViewSet,
+                basename='project')
 router.register('keyword',
                 # OLD: 'keywords'
-                KeywordViewSet)
+                KeywordViewSet,
+                basename='keyword')
 router.register('digest-issue',
                 # OLD: 'digest-issues'
-                DigestIssueViewSet)
+                DigestIssueViewSet,
+                basename='digest-issue')
 router.register('content-category/guess',
                 # OLD: 'guess-content-category'
-                GuessContentCategoryView)
+                GuessContentCategoryView,
+                basename='content-category/guess')
 # TODO: Rewrite ViewSet in style similar to other new endpoints
 router.register('similar-records-in-previous-digest',
-                SimilarRecordsInPreviousDigest)
+                SimilarRecordsInPreviousDigest,
+                basename='similar-records-in-previous-digest')
 
 urlpatterns += router.urls
--- fngs/tbot/urls_v2.py
+++ fngs/tbot/urls_v2.py
@@ -4,27 +4,35 @@ from tbot.views import *
 router = routers.DefaultRouter()
 router.register('user',
                 # OLD: 'telegram-bot-user'
-                TelegramBotUserViewSet)
+                TelegramBotUserViewSet,
+                basename='user')
 router.register('user/detailed',
                 # OLD: 'telegram-bot-user-detailed'
-                TelegramBotUserDetailedViewSet)
+                TelegramBotUserDetailedViewSet,
+                basename='user/detailed')
 router.register('group',
                 # OLD: 'telegram-bot-user-group'
-                TelegramBotUserGroupViewSet)
+                TelegramBotUserGroupViewSet,
+                basename='group')
 router.register('digest-record/categorization-attempt',
                 # OLD: 'telegram-bot-digest-record-categorization-attempt'
-                TelegramBotDigestRecordCategorizationAttemptViewSet)
+                TelegramBotDigestRecordCategorizationAttemptViewSet,
+                basename='digest-record/categorization-attempt')
 router.register('digest-record/categorization-attempt/detailed',
                 # OLD: 'telegram-bot-digest-record-categorization-attempt-detailed'
-                TelegramBotDigestRecordCategorizationAttemptDetailedViewSet)
+                TelegramBotDigestRecordCategorizationAttemptDetailedViewSet,
+                basename='digest-record/categorization-attempt/detailed')
 router.register('digest-record/not-categorized/random',
                 # OLD: 'telegram-bot-one-random-not-categorized-foss-news-digest-record'
-                TelegramBotOneRandomNotCategorizedDigestRecordViewSet)
+                TelegramBotOneRandomNotCategorizedDigestRecordViewSet,
+                basename='digest-record/not-categorized/random')
 router.register('digest-record/not-categorized/count',
                 # OLD: 'telegram-bot-not-categorized-foss-news-digest-records-count'
-                TelegramBotNotCategorizedDigestRecordsCountViewSet)
+                TelegramBotNotCategorizedDigestRecordsCountViewSet,
+                basename='digest-record/not-categorized/count')
 router.register('digest-record/categorized',
                 # OLD: 'digest-records-categorized-by-tbot'
-                DigestRecordsCategorizedByTbotViewSet)
+                DigestRecordsCategorizedByTbotViewSet,
+                basename='digest-record/categorized')
 
 urlpatterns = router.urls
