ab63b8d258ac87430a76e71caeabf143205883b8
Dmitriy Vinokurov <gim6626@gmail.com>
2021-12-25 10:33:36 +0500

    TG-89 Fixed incomplete queryset for partially categorized records

--- fngs/gatherer/mixins.py
+++ fngs/gatherer/mixins.py
@@ -7,19 +7,19 @@ from tbot.models import *
 class NotCategorizedDigestRecordsMixin:
 
     def not_categorized_records_queryset(self, from_bot: bool, project_name: str = 'FOSS News'):
-        queryset = DigestRecord.objects.filter(projects__in=(Project.objects.filter(name=project_name)))
-        unknown_state_queryset = queryset.filter(state='UNKNOWN')
+        dt_now = datetime.datetime.now()
+        dt_now_minus_1m = dt_now - datetime.timedelta(days=30)
+        recent_foss_news_records = DigestRecord.objects.filter(projects__in=(Project.objects.filter(name=project_name)), gather_dt__gt=dt_now_minus_1m)
+        unknown_state_queryset = recent_foss_news_records.filter(state='UNKNOWN')
+        unknown_content_type_queryset = recent_foss_news_records.filter(content_type='UNKNOWN')
+        none_content_type_queryset = recent_foss_news_records.filter(content_type=None)
+        none_is_main_queryset = recent_foss_news_records.filter(is_main=None)
+        none_content_category_queryset = recent_foss_news_records.filter(content_category=None).exclude(content_type='OTHER')
+        partially_categorized_queryset = unknown_state_queryset | unknown_content_type_queryset | none_content_type_queryset | none_is_main_queryset | none_content_category_queryset
         if from_bot:
-            dt_now = datetime.datetime.now()
-            dt_now_minus_2w = dt_now - datetime.timedelta(days=14)
-            recent_tbot_attempts = TelegramBotDigestRecordCategorizationAttempt.objects.filter(dt__gt=dt_now_minus_2w)
+            recent_tbot_attempts = TelegramBotDigestRecordCategorizationAttempt.objects.filter(dt__gt=dt_now_minus_1m)
             recent_tbot_attempts_records_ids = [attempt.digest_record.id for attempt in recent_tbot_attempts]
-            queryset = unknown_state_queryset.filter(id__in=recent_tbot_attempts_records_ids)
-            return queryset
+            recent_foss_news_records = partially_categorized_queryset.filter(id__in=recent_tbot_attempts_records_ids)
+            return recent_foss_news_records
         else:
-            foss_news_current_queryset = queryset.filter(digest_issue__number=DigestIssue.objects.order_by('-number')[0].number, state='IN_DIGEST')
-            unknown_content_type_queryset = foss_news_current_queryset.filter(content_type='UNKNOWN')
-            none_content_type_queryset = foss_news_current_queryset.filter(content_type=None)
-            none_is_main_queryset = foss_news_current_queryset.filter(is_main=None)
-            none_content_category_queryset = foss_news_current_queryset.filter(content_category=None).exclude(content_type='OTHER')
-            return unknown_state_queryset | unknown_content_type_queryset | none_content_type_queryset | none_is_main_queryset | none_content_category_queryset
+            return partially_categorized_queryset.filter(digest_issue__number=DigestIssue.objects.order_by('-number')[0].number, state='IN_DIGEST')
