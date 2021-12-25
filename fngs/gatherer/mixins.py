5f71f90fcf0e5b09aeeb28a3198fba0128b851a3
Dmitriy Vinokurov <gim6626@gmail.com>
2021-12-25 12:32:52 +0500

    TG-89 Excluding badly categorized records from queryset from tbot

--- fngs/gatherer/mixins.py
+++ fngs/gatherer/mixins.py
@@ -10,16 +10,21 @@ class NotCategorizedDigestRecordsMixin:
         dt_now = datetime.datetime.now()
         dt_now_minus_1m = dt_now - datetime.timedelta(days=30)
         recent_foss_news_records = DigestRecord.objects.filter(projects__in=(Project.objects.filter(name=project_name)), gather_dt__gt=dt_now_minus_1m)
+        bad_records = recent_foss_news_records.filter(state__in=(DigestRecordState.SKIPPED.name,
+                                                                 DigestRecordState.IGNORED.name,
+                                                                 DigestRecordState.FILTERED.name,
+                                                                 DigestRecordState.OUTDATED.name,
+                                                                 DigestRecordState.DUPLICATE.name))
         unknown_state_queryset = recent_foss_news_records.filter(state='UNKNOWN')
-        unknown_content_type_queryset = recent_foss_news_records.filter(content_type='UNKNOWN')
-        none_content_type_queryset = recent_foss_news_records.filter(content_type=None)
-        none_is_main_queryset = recent_foss_news_records.filter(is_main=None)
-        none_content_category_queryset = recent_foss_news_records.filter(content_category=None).exclude(content_type='OTHER')
+        unknown_content_type_queryset = recent_foss_news_records.filter(content_type='UNKNOWN').exclude(pk__in=bad_records)
+        none_content_type_queryset = recent_foss_news_records.filter(content_type=None).exclude(pk__in=bad_records)
+        none_is_main_queryset = recent_foss_news_records.filter(is_main=None).exclude(pk__in=bad_records)
+        none_content_category_queryset = recent_foss_news_records.filter(content_category=None).exclude(content_type='OTHER').exclude(pk__in=bad_records)
         partially_categorized_queryset = unknown_state_queryset | unknown_content_type_queryset | none_content_type_queryset | none_is_main_queryset | none_content_category_queryset
         if from_bot:
             recent_tbot_attempts = TelegramBotDigestRecordCategorizationAttempt.objects.filter(dt__gt=dt_now_minus_1m)
             recent_tbot_attempts_records_ids = [attempt.digest_record.id for attempt in recent_tbot_attempts]
-            recent_foss_news_records = partially_categorized_queryset.filter(id__in=recent_tbot_attempts_records_ids)
-            return recent_foss_news_records
+            recent_partially_categorized_foss_news_records = partially_categorized_queryset.filter(id__in=recent_tbot_attempts_records_ids)
+            return recent_partially_categorized_foss_news_records
         else:
             return partially_categorized_queryset.filter(digest_issue__number=DigestIssue.objects.order_by('-number')[0].number, state='IN_DIGEST')
